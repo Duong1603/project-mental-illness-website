f8679175bb9b822dd956966945ffe319a7c6ac42
tranvanhieu01012002 <hieu.tran23@student.passerellesnumeriques.org>
2022-07-14 08:09:38 +0700

    finish create table

---
+++ database/migrations/2022_07_11_022510_create_web_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('web_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('gender',array('male','female','other'));
+            $table->string('phone');
+            $table->string('mail');
+            $table->date('dob');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('web_users');
+    }
+};

db4a75a85fcb7b72dd70b38819bb0025a153abfc
tranvanhieu01012002 <hieu.tran23@student.passerellesnumeriques.org>
2022-07-26 15:32:00 +0700

    create a seed to run on Postgresql

---
+++ database/seeders/Postgresql.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Postgresql extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\Models\Category::factory(10)->create();
+        \App\Models\Post::factory(10)->create();
+        \App\Models\User::factory(100)->create();
+        \App\Models\Comment::factory(10)->create();
+
+        \App\Models\Contact::factory(100)->create();
+
+        \App\Models\Package::factory(3)->create();
+        \App\Models\Doctor::factory(2)->create();
+
+
+        \App\Models\Order::factory(10)->create();
+        \App\Models\Booking::factory(10)->create();
+
+        \App\Models\Role::factory(2)->create();
+
+        \App\Models\Admin::factory(2)->create();
+
+    }
+}

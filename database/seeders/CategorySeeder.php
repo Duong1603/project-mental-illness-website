1d9a810518fcd4a18997f46927ccb94fb23c2b0e
tranvanhieu01012002 <hieu.tran23@student.passerellesnumeriques.org>
2022-07-30 08:40:09 +0700

    un block seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,24 +15,25 @@ class CategorySeeder extends Seeder
     public function run()
     {
         //
-        // \App\Models\Category::factory(10)->create();
-        // \App\Models\Post::factory(10)->create();
-        // \App\Models\User::factory(100)->create();
-        // \App\Models\Comment::factory(10)->create();
+        \App\Models\Category::factory(10)->create();
+        \App\Models\Post::factory(10)->create();
+        \App\Models\User::factory(100)->create();
+        \App\Models\Comment::factory(10)->create();
 
-        // \App\Models\Contact::factory(100)->create();
+        \App\Models\Contact::factory(100)->create();
 
-        // \App\Models\Package::factory(3)->create();
-        // \App\Models\Doctor::factory(2)->create();
+        \App\Models\Package::factory(3)->create();
+        \App\Models\Doctor::factory(2)->create();
 
 
-        // \App\Models\Order::factory(10)->create();
+        \App\Models\Order::factory(10)->create();
+        
         \App\Models\Booking::factory(10)->create();
 
-        // \App\Models\Role::factory(2)->create();
-        // \App\Models\UsersRole::factory(10)->create();
+        \App\Models\Role::factory(2)->create();
+        \App\Models\UsersRole::factory(10)->create();
 
-        // \App\Models\Admin::factory(2)->create();
+        \App\Models\Admin::factory(2)->create();
 
     }
 }

57d08244321f07461dc8d3ae515856659f2d16f3
tranvanhieu01012002 <hieu.tran23@student.passerellesnumeriques.org>
2022-08-05 21:59:39 +0700

    update authenticated to login

--- routes/web.php
+++ routes/web.php
@@ -7,72 +7,25 @@ use App\Http\Controllers\Admin\BookingController;
 use App\Http\Controllers\Admin\OverviewController;
 use App\Http\Controllers\Admin\SendEmailController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
-// OverView
-// Route::group([
-//     'namespace' => 'Admin',
-//     'prefix' => 'admin/overview',
-//     // 'middleware' => ['auth']
-// ], function () {
-//     Route::get('/', [OverviewController::class, "index"])->name('overview.index');
-// });
+Route::get('/login', [AdminController::class, "getLogin"])->name('admin.getLogin');
+Route::post('/login', [AdminController::class, "postLogin"])->name('admin.postLogin');
+Route::get('/logout', [AdminController::class, "getLogout"])->name('admin.getLogout');
 
-// // ManagerCustomer
-// Route::group([
-//     'namespace' => 'Admin',
-//     'prefix' => 'admin/managerCustomer',
-//     // 'middleware' => ['auth']
-// ], function () {
-
-// });
-
-// Blogs
-
-
-
-// login
-// Route::group([
-//     'namespace' => 'Admin',
-//     'prefix' => 'admin',
-//     // 'middleware' => ['auth']
-// ], function () {
-//     Route::get('/logout', [UserController::class, 'Logout']);
-//     Route::post('/login', [UserController::class, 'Login']);
-//     // Route::post('/register', [UserController::class, 'Register']);
-//     Route::get('/login', [UserController::class, "index"])->name('login.index');
-// });
-
-Route::get('/logout', [AdminController::class, 'Logout']);
-Route::post('/login', [AdminController::class, 'Login']);
-// Route::post('/register', [UserController::class, 'Register']);
-Route::get('/', [AdminController::class, "index"])->name('login.index');
-
-
-
-
-
-Route::get('/contact', [SendEmailController::class, 'index']);
-Route::post('/send', [SendEmailController::class, 'send'])->name('email.send');
-Route::get('/admin/login', [AdminController::class, "getLogin"])->name('admin.getLogin');
-Route::post('/admin/login', [AdminController::class, "postLogin"])->name('admin.postLogin');
 Route::group([
     "namespace" => "Admin",
     "middleware" => "AuthAdmin",
     "prefix" => "/admin"
 ], function () {
 
-    // Overview
     Route::group(["prefix" => "overview"], function () {
         Route::get('/', [OverviewController::class, "index"])->name('overview.index');
     });
 
-    // Manager customer
     Route::group(["prefix" => "managerCustomer"], function () {
         Route::get('/', [UserController::class, "index"])->name('managerCustomer.index');
     });
 
-    //Manager blogs
     Route::group([
         'prefix' => '/blogs',
     ], function () {
@@ -84,12 +37,11 @@ Route::group([
         Route::get('/', [PostController::class, "index"])->name('posts.index');
     });
 
-    //Manager bookings
     Route::group([
         'prefix' => '/bookings',
     ], function () {
         Route::get('/', [BookingController::class, "index"])->name('bookings.index');
     });
 });
-Route::get('/contact', [SendEmailController::class, 'index']);
-Route::post('/send', [SendEmailController::class, 'send'])->name('email.send');
+// Route::get('/contact', [SendEmailController::class, 'index']);
+// Route::post('/send', [SendEmailController::class, 'send'])->name('email.send');

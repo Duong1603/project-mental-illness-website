d51f8452ae471861c9a97f575b7a4ab587b6311f
Liam Nguyen <liam.n.sapotacorp@goshopmatic.com>
2022-08-19 21:05:31 +0700

    fix send mail for admin

--- config/constants.php
+++ config/constants.php
@@ -10,6 +10,7 @@ return [
     'RESIGN_EMAIL'=> '[Phi Phi] Successful scheduling',
     'CHANGE_TIME_EMAIL'=> '[Phi Phi] Change meeting time',
     'QR_CODE_PACKAGE_2_HOURS' => 'qr-code/code1.png',
-    'QR_CODE_PACKAGE_6_HOURS' => 'qr-code/code2.jng'
+    'QR_CODE_PACKAGE_6_HOURS' => 'qr-code/code2.jng',
+    'ADMIN_EMAIL'=>env('ADMIN_EMAIL','hieu.tran23@student.passerellesnumeriques.org')
     ]
 ?>
